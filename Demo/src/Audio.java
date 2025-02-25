5d10698f2da5d0a2c92fac126a0de60dfdc32a74
Anawin A. <138203645+AnawinA@users.noreply.github.com>
2025-02-25 12:11:39 +0700

    add 9 audios

---
+++ Demo/src/Audio.java
@@ -0,0 +1,45 @@
+
+import java.io.File;
+import javax.lang.model.element.Element;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author anawi
+ */
+public class Audio {
+    private static final String AUDIO_FOLDER = "src\\audios";
+    
+    public static void play(String name) {
+        if (!name.contains(".wav")) {
+            name += ".wav";
+        }
+        File file = new File(AUDIO_FOLDER, name);
+        new Thread(() -> {
+            try (AudioInputStream sound = AudioSystem.getAudioInputStream(file);) {
+                Clip clip = AudioSystem.getClip();
+                clip.open(sound);
+                clip.start();
+                Thread.sleep(500);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+    
+    public static void main(String[] args) {
+        Audio.play(AudioName.FIRE);
+        Audio.play(AudioName.HIT);
+        Audio.play(AudioName.PLANT_PICK_UP);
+        Audio.play(AudioName.PLANT_DELETE);
+        Audio.play(AudioName.PLANT_PLACE);
+    }
+    
+}
---
+++ Demo/src/AudioName.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author anawi
+ */
+public interface AudioName {
+    String FIRE = "fire";
+    String FIRE_TINY = "fire-tiny";
+    String HIT = "bullet-hit";
+    String KILL = "kill";
+    String KILL2 = "kill2";
+    String PLANT_PICK_UP = "pick-up-plant";
+    String PLANT_PLACE = "place-plant";
+    String PLANT_DELETE = "delete-plant";
+    String PLANT_CANT_PICK_UP = "cant-pick-up-plant";
+    String BTN_SELECT = "btn-select";
+}
