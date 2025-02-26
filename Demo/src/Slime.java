6a9489c3b4301bfef195c17f1a16639e28df1534
Harley2zaza <67070259@kmitl.ac.th>
2025-02-26 19:52:34 +0700

    Animate Slime

--- Demo/src/Slime.java
+++ Demo/src/Slime.java
@@ -1,4 +1,5 @@
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.List;
 import javax.imageio.ImageIO;
@@ -16,7 +17,10 @@ public class Slime extends Unit {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        costGenerationTimer = new Timer(10000, e -> generateCost()); 
+        costGenerationTimer = new Timer(10000, e -> {
+            generateCost();
+        }
+        );
         costGenerationTimer.start();
     }
 
@@ -26,10 +30,20 @@ public class Slime extends Unit {
 
     // Add 50 cost every 10 seconds
     public void generateCost() {
+        this.currentFrame = 0;
+        this.Status = "ATK";
         GamePanel.remainMana += 50;
         if (GamePanel.remainMana > GamePanel.MAX_MANA) {
             GamePanel.remainMana = GamePanel.MAX_MANA;
         }
+        new Thread(() -> {try{ //delay Time
+            Thread.sleep(1000);
+            this.currentFrame = 0;
+            this.Status ="idle";
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        }).start();
     }
 
     @Override
@@ -46,8 +60,22 @@ public class Slime extends Unit {
     public void attack(List<Bullet> bullets) {}
 
     @Override
+    public BufferedImage getBufferedImage() {
+        if (this.Status.equals("idle")){
+            return actionIdle.getSubimage(currentFrame * frame_Width, 0, frame_Width, frame_Hight);}
+        else {
+            return actionATK.getSubimage(currentFrame * frame_Width, 0, frame_Width, frame_Hight);
+        }
+    }
+    
+    @Override
     public void updateFrame() {
-        super.updateFrame();
+        if (this.Status.equals("idle")){
+            currentFrame = (currentFrame + 1) % total_Frame_Idle;
+        }
+        else {
+            currentFrame = (currentFrame + 1) % total_Frame_ATK;
+        }
     }
 
 }
