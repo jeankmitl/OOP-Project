d7d9305aa96f0c2ff2cf55d8f234968024a21333
Harley2zaza <67070259@kmitl.ac.th>
2025-03-13 18:36:30 +0700

    Add Loading Screen

--- Demo/src/Main/LoadingScreen.java
+++ Demo/src/Main/LoadingScreen.java
@@ -9,11 +9,19 @@ public class LoadingScreen extends JFrame {
         setSize(1280,720);
         setLocationRelativeTo(null);
         setUndecorated(true);
-        setLayout(new BorderLayout());
-
-        JLabel loadingLabel = new JLabel("Loading...", SwingConstants.CENTER);
-        loadingLabel.setFont(new Font("Algerian", Font.BOLD, 100));
-        add(loadingLabel, BorderLayout.CENTER);
+//        setLayout(new BorderLayout());
+        
+        
+        JPanel pr = new JPanel();
+        pr.setBackground(new Color(55, 55, 71));
+        pr.setLayout(new BorderLayout());
+        
+        ImageIcon load = new ImageIcon("src/Asset/Img/Icons/LoadingScreen.gif");
+        JLabel loadingscreen = new JLabel(load, SwingConstants.CENTER);
+        loadingscreen.setHorizontalAlignment(SwingConstants.CENTER);
+        loadingscreen.setVerticalAlignment(SwingConstants.CENTER);
+        pr.add(loadingscreen, BorderLayout.CENTER);
+        add(pr);
         setVisible(true);
     }
     
