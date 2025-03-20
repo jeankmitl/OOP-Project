37fee9a3ba3f85d2804c75c636b859022c1284e8
Harley2zaza <67070259@kmitl.ac.th>
2025-03-20 20:28:54 +0700

    Stage 3 Can Playable Too

--- Demo/src/Main/stage3.java
+++ Demo/src/Main/stage3.java
@@ -4,15 +4,57 @@
  */
 package Main;
 
+import DSystem.DWait;
+import Entities.Enemies.Bandit;
+import Entities.Enemies.BanditV2;
+import Entities.Enemies.BanditV3;
+
 /**
  *
  * @author USER
  */
 public class stage3 extends GamePanel{
-
+    protected DWait start,w1,w2,w3,w4,w5;
+    
     public stage3() {
-        super(38);
+        super(34);
     }
-    
-    
+
+    @Override
+    public void summonEnemies() {
+        start = new DWait(10, e->{//2
+            System.out.println("Wave 1");
+            Spawn_Enemy(new Bandit(0,0),2,8); //1
+            w2.start();
+        });
+        
+        w2 = new DWait(20, e->{
+            System.out.println("Wave 2");
+            Spawn_Enemy(new Bandit(0,0), 3, 5); //3
+            w3.start();
+        });
+        
+        w3 = new DWait(20, e->{
+            System.out.println("Wave 3");
+            Spawn_Enemy(new Bandit(0,0), 4,3); //5
+            Spawn_Enemy(new BanditV2(0, 0),2,5);
+            w4.start();
+        });
+        
+        w4 = new DWait(30, e->{
+            System.out.println("Wave 4");
+            Spawn_Enemy(new Bandit(0,0), 4, 8); //9
+            Spawn_Enemy(new BanditV2(0,0), 3, 6);
+            Spawn_Enemy(new BanditV3(0,0),2,12);
+            w5.start();
+        });
+        
+        w5 = new DWait(40, e->{
+            System.out.println("Wave Final"); //14
+            Spawn_Enemy(new Bandit(0,0),6,4);
+            Spawn_Enemy(new BanditV2(0,0),5,3);
+            Spawn_Enemy(new BanditV3(0,0),3,8); 
+        });
+        start.start();
+    }  
 }
