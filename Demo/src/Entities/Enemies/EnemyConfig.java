ee84323833ec3c5c03d8571e62a496296bf6dca5
Wanlachai Klammuang <147297706+GEOFFCHARGE@users.noreply.github.com>
2025-03-22 10:37:23 +0700

    Balance V1.4

--- Demo/src/Entities/Enemies/EnemyConfig.java
+++ Demo/src/Entities/Enemies/EnemyConfig.java
@@ -27,15 +27,15 @@ public interface EnemyConfig {
     EnemySpriteSheets RC_BOMBER_SPRITE = new EnemySpriteSheets("rc_bomber", "rc_bomber");
     EnemySpriteSheets ROBOT_MONOWHEEL_SPRITE = new EnemySpriteSheets("robot_monowheel_idle", "robot_monowheel_atk");
     EnemySpriteSheets TANK_SPRITE = new EnemySpriteSheets("tank", "tank");
-    
+
     // All Unit stats 💡
-    EnemyStats NINJA_STATS = new EnemyStats(NINJA_SPRITE, 335, 125, 1, WALK_NORMAL, NORMAL);
-    EnemyStats BANDIT_STATS = new EnemyStats(BANDIT_SPRITE, 181, 100, 1, WALK_NORMAL, LONG_RANGE);
-    EnemyStats BANDIT_STATSV2 = new EnemyStats(BANDIT_SPRITEV2, 551, 100, 1, WALK_NORMAL, LONG_RANGE);
+    EnemyStats NINJA_STATS = new EnemyStats(NINJA_SPRITE, 340, 100, 1, WALK_NORMAL, NORMAL);
+    EnemyStats BANDIT_STATS = new EnemyStats(BANDIT_SPRITE, 190, 100, 1, WALK_NORMAL, LONG_RANGE);
+    EnemyStats BANDIT_STATSV2 = new EnemyStats(BANDIT_SPRITEV2, 560, 100, 1, WALK_NORMAL, LONG_RANGE);
     EnemyStats BANDIT_STATSV3 = new EnemyStats(BANDIT_SPRITEV3, 1290, 100, 1, WALK_NORMAL, LONG_RANGE);
-    EnemyStats SORCERER_STATS = new EnemyStats(SORCERER_SPRITE, 301, 100, 1, WALK_SLOW, NORMAL);
-    EnemyStats LITTLE_RED_HOOD_STATS = new EnemyStats(LITTLE_RED_HOOD_SPRITE, 801, 125, 1, WALK_FAST, NORMAL);
+    EnemyStats SORCERER_STATS = new EnemyStats(SORCERER_SPRITE, 190, 100, 1, WALK_SLOW, NORMAL);
+    EnemyStats LITTLE_RED_HOOD_STATS = new EnemyStats(LITTLE_RED_HOOD_SPRITE, 550, 100, 1, WALK_FAST, NORMAL);
     EnemyStats RC_BOMBER_STATS = new EnemyStats(RC_BOMBER_SPRITE, 50, 2000, 1, WALK_CAR, NORMAL);
-    EnemyStats ROBOT_MONOWHEEL_STATS = new EnemyStats(ROBOT_MONOWHEEL_SPRITE, 300, 150, 1, WALK_NORMAL, NORMAL);
-    EnemyStats TANK_STATS = new EnemyStats(TANK_SPRITE, 1000, 20, 0.1, WALK_SLOW, NORMAL);
+    EnemyStats ROBOT_MONOWHEEL_STATS = new EnemyStats(ROBOT_MONOWHEEL_SPRITE, 190, 100, 1, WALK_NORMAL, NORMAL);
+    EnemyStats TANK_STATS = new EnemyStats(TANK_SPRITE, 2200, 100, 0.1, WALK_SLOW, NORMAL);
 }
