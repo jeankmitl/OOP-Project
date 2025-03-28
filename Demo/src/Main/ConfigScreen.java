9e3bea725cee46c36dacc3152a59f51e9eaa5c21
Wanlachai Klammuang <147297706+GEOFFCHARGE@users.noreply.github.com>
2025-03-28 16:47:15 +0700

    Move musicBtn and soundBtn to ConfigScreen

--- Demo/src/Main/ConfigScreen.java
+++ Demo/src/Main/ConfigScreen.java
@@ -3,7 +3,6 @@ package Main;
 import Asset.Audio;
 import Asset.AudioName;
 import Asset.ImgManager;
-
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
@@ -12,25 +11,37 @@ import javax.swing.*;
 
 public class ConfigScreen extends JFrame {
 
+    private Rectangle homeBtn, musicBtn, soundBtn;
+    private Image background, musicImg, soundImg;
     private boolean isButtonHovered = false;
     private ConfigScreenPanel configScreenPanel;
-    private Image background;
-    private Rectangle homeBtn;
 
     public ConfigScreen() {
 
+        homeBtn = new Rectangle(1170, 15, 75, 75);
+        musicBtn = new Rectangle(462, 180, 160, 160);
+        soundBtn = new Rectangle(652, 180, 160, 160);
         configScreenPanel = new ConfigScreenPanel();
-        add(configScreenPanel);
 
         try {
             setIconImage(new ImageIcon(getClass().getResource("/Asset/Img/Icons/icon.png")).getImage());
-            background = ImageIO.read(getClass().getResource("/Asset/Img/Background/defense_of_dungeon_wallpaper.png")); // Initialize background
-        } catch (IOException e) {
+            background = ImageIO.read(getClass().getResource("/Asset/Img/Background/defense_of_dungeon_wallpaper.png"));
+            soundImg = ImgManager.loadIcon((Audio.isSoundEnable()) ? "unmute_sound" : "mute_sound");
+            musicImg = ImgManager.loadIcon((Audio.isMusicEnable()) ? "unmute_music" : "mute_music");
+        }
+        catch (IOException e) {
             e.printStackTrace();
             System.out.println("Wrong image path.");
         }
 
-        homeBtn = new Rectangle(1170, 15, 75, 75);
+        add(configScreenPanel);
+
+        setTitle("Config");
+        setSize(1264, 681);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setUndecorated(true);
+        setVisible(true);
 
         addMouseListener(new MouseAdapter() {
             @Override
@@ -54,13 +65,37 @@ public class ConfigScreen extends JFrame {
                     };
                     worker.execute();
                 }
+                else if (musicBtn.contains(e.getPoint())) {
+                    if (Audio.isMusicEnable()) {
+                        Audio.setMusicEnable(false);
+                        Audio.stopMusic();
+                        musicImg = ImgManager.loadIcon("mute_music");
+                    }
+                    else {
+                        Audio.setMusicEnable(true);
+                        Audio.playMusic("mainMenu");
+                        musicImg = ImgManager.loadIcon("unmute_music");
+                    }
+                    configScreenPanel.repaint();
+                }
+                else if (soundBtn.contains(e.getPoint())) {
+                    if (Audio.isSoundEnable()) {
+                        Audio.setSoundEnable(false);
+                        soundImg = ImgManager.loadIcon("mute_sound");
+                    }
+                    else {
+                        Audio.setSoundEnable(true);
+                        soundImg = ImgManager.loadIcon("unmute_sound");
+                    }
+                    configScreenPanel.repaint();
+                }
             }
         });
 
         addMouseMotionListener(new MouseMotionAdapter() {
             @Override
             public void mouseMoved(MouseEvent e) {
-                if (homeBtn.contains(e.getPoint())) {
+                if (homeBtn.contains(e.getPoint()) || musicBtn.contains(e.getPoint()) || soundBtn.contains(e.getPoint())) {
                     setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
                     if (!isButtonHovered) {
                         isButtonHovered = true;
@@ -74,23 +109,18 @@ public class ConfigScreen extends JFrame {
             }
         });
 
-        setTitle("Config");
-        setSize(1264, 681);
-        setLocationRelativeTo(null);
-        setUndecorated(true);
-        setVisible(true);
-
     }
 
     private class ConfigScreenPanel extends JPanel {
         @Override
         public void paintComponent(Graphics g) {
-            super.paintComponent(g);
             Graphics2D g2d = (Graphics2D) g;
             Image homeIcon = ImgManager.loadIcon("home_Btn");
             g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             g2d.drawImage(background, 0, 0, 1264, 681, this);
             g2d.drawImage(homeIcon, homeBtn.x, homeBtn.y, homeBtn.width, homeBtn.height, this);
+            g2d.drawImage(musicImg, musicBtn.x, musicBtn.y, musicBtn.width, musicBtn.height, this);
+            g2d.drawImage(soundImg, soundBtn.x, soundBtn.y, soundBtn.width, soundBtn.height, this);
         }
     }
 }
--- Demo/src/Main/MainMenu.java
+++ Demo/src/Main/MainMenu.java
@@ -2,16 +2,16 @@ package Main;
 
 import Asset.Audio;
 import Asset.AudioName;
-import Asset.ImgManager;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.IOException;
-import javax.imageio.ImageIO;
+import java.io.*;
+import javax.imageio.*;
 import javax.swing.*;
 
 public class MainMenu extends JFrame {
-    private Rectangle start, twoP, config, exit, menuBar, logo, musicBtn, soundBtn;
-    private Image menu, title, background, musicImg, soundImg;
+
+    private Rectangle start, twoP, config, exit, menuBar, logo;
+    private Image menu, title, background;
     private boolean isButtonHovered = false;
     private Timer timer;
     private MainMenuPanel mainMenuPanel;
@@ -24,8 +24,6 @@ public class MainMenu extends JFrame {
         twoP = new Rectangle(75, 215, 300, 110);
         config = new Rectangle(75, 350, 300, 115);
         exit = new Rectangle(75, 485, 300, 115);
-        musicBtn = new Rectangle(1170, 15, 80, 80);
-        soundBtn = new Rectangle(1085, 15, 80, 80);
         mainMenuPanel = new MainMenuPanel();
 
         try {
@@ -33,15 +31,14 @@ public class MainMenu extends JFrame {
             menu = ImageIO.read(getClass().getResource("/Asset/Img/Icons/MenuBar.PNG"));
             title = ImageIO.read(getClass().getResource("/Asset/Img/Icons/defense_of_the_dungeon_no_s.png"));
             background = ImageIO.read(getClass().getResource("/Asset/Img/Background/defense_of_dungeon_wallpaper.png"));
-            soundImg = ImgManager.loadIcon((Audio.isSoundEnable()) ? "unmute_sound" : "mute_sound");
-            musicImg = ImgManager.loadIcon((Audio.isMusicEnable()) ? "unmute_music" : "mute_music");
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
             System.out.println("Wrong image path.");
         }
+
         add(mainMenuPanel);
-        
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
         setTitle("Defense of the Dungeon");
         setSize(1264, 681);
         setResizable(false);
@@ -73,7 +70,8 @@ public class MainMenu extends JFrame {
                         }
                     };
                     worker.execute();
-                } else if (twoP.contains(e.getPoint())) {
+                }
+                else if (twoP.contains(e.getPoint())) {
                     Audio.play(AudioName.BUTTON_CLICK);
                     System.out.println("Choose the stage you want");
 
@@ -94,7 +92,8 @@ public class MainMenu extends JFrame {
                         }
                     };
                     worker.execute();
-                } else if (config.contains(e.getPoint())) {
+                }
+                else if (config.contains(e.getPoint())) {
                     Audio.play(AudioName.BUTTON_CLICK);
                     LoadingScreen loadingScreen = new LoadingScreen();
                     SwingWorker<Void, Void> worker = new SwingWorker<>() {
@@ -113,45 +112,28 @@ public class MainMenu extends JFrame {
                         }
                     };
                     worker.execute();
-                } else if (exit.contains(e.getPoint())) {
+                }
+                else if (exit.contains(e.getPoint())) {
                     int res = JOptionPane.showConfirmDialog(mainMenuPanel, "Do you want to exit the game?", "Exit game", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
-                        if (res == JOptionPane.YES_OPTION) {
-                            Audio.play(AudioName.BUTTON_CLICK);
-                            LoadingScreen loadingScreen = new LoadingScreen();
-                            SwingWorker<Void, Void> worker = new SwingWorker<>() {
-                                @Override
-                                protected Void doInBackground() throws Exception {
-                                    dispose();
-                                    Thread.sleep(1500);
-                                    return null;
-                                }
+                    if (res == JOptionPane.YES_OPTION) {
+                        Audio.play(AudioName.BUTTON_CLICK);
+                        LoadingScreen loadingScreen = new LoadingScreen();
+                        SwingWorker<Void, Void> worker = new SwingWorker<>() {
+                            @Override
+                            protected Void doInBackground() throws Exception {
+                                dispose();
+                                Thread.sleep(1500);
+                                return null;
+                            }
 
-                                @Override
-                                protected void done() {
-                                    System.out.println("Good Bye Bro");
-                                    loadingScreen.dispose();
-                                    System.exit(0);
-                                }
-                            };
-                            worker.execute();
-                        }
-                } else if (musicBtn.contains(e.getPoint())) {
-                    if (Audio.isMusicEnable()) {
-                        Audio.setMusicEnable(false);
-                        Audio.stopMusic();
-                        musicImg = ImgManager.loadIcon("mute_music");
-                    } else {
-                        Audio.setMusicEnable(true);
-                        Audio.playMusic("mainMenu");
-                        musicImg = ImgManager.loadIcon("unmute_music");
-                    }
-                } else if (soundBtn.contains(e.getPoint())) {
-                    if (Audio.isSoundEnable()) {
-                        Audio.setSoundEnable(false);
-                        soundImg = ImgManager.loadIcon("mute_sound");
-                    } else {
-                        Audio.setSoundEnable(true);
-                        soundImg = ImgManager.loadIcon("unmute_sound");
+                            @Override
+                            protected void done() {
+                                System.out.println("Bye Bye Bro");
+                                loadingScreen.dispose();
+                                System.exit(0);
+                            }
+                        };
+                        worker.execute();
                     }
                 }
             }
@@ -160,24 +142,20 @@ public class MainMenu extends JFrame {
         addMouseMotionListener(new MouseMotionAdapter() {
             @Override
             public void mouseMoved(MouseEvent e) {
-                if (start.contains(e.getPoint()) ||
-                    twoP.contains(e.getPoint()) ||
-                    config.contains(e.getPoint()) ||
-                    exit.contains(e.getPoint()) ||
-                    musicBtn.contains(e.getPoint()) ||
-                    soundBtn.contains(e.getPoint())) {
+                if (start.contains(e.getPoint()) || twoP.contains(e.getPoint()) || config.contains(e.getPoint()) || exit.contains(e.getPoint())) {
                     setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
                     if (!isButtonHovered) {
                         isButtonHovered = true;
                         Audio.play(AudioName.BUTTON_HOVER);
                     }
-                } else {
+                }
+                else {
                     setCursor(Cursor.getDefaultCursor());
                     isButtonHovered = false;
                 }
             }
         });
-        
+
         addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
@@ -190,15 +168,15 @@ public class MainMenu extends JFrame {
                         break;
                 }
             }
-            
         });
+
         timer = new Timer(32, new Animation());
         timer.start();
         
         Audio.playMusic("mainMenu");
     }
-    
-    
+
+
     private int titleAnimY = 0;
     private class Animation implements ActionListener {
         @Override
@@ -207,7 +185,7 @@ public class MainMenu extends JFrame {
             repaint();
         }
     }
-    
+
     private class MainMenuPanel extends JPanel {
         @Override
         public void paintComponent(Graphics g) {
@@ -216,8 +194,7 @@ public class MainMenu extends JFrame {
             g2d.drawImage(background, 0, 0, 1280, 720,this);
             g2d.drawImage(menu, menuBar.x, menuBar.y, menuBar.width, menuBar.height,this);
             g2d.drawImage(title, logo.x, logo.y + (int)(20 * Math.sin(Math.toRadians(titleAnimY))), logo.width, logo.height,this);
-            g2d.drawImage(musicImg, musicBtn.x, musicBtn.y, musicBtn.width, musicBtn.height, this);
-            g2d.drawImage(soundImg, soundBtn.x, soundBtn.y, soundBtn.width, soundBtn.height, this);
         }
     }
+
 }
