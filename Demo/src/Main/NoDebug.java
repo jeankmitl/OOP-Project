2234533d4252fcee97fac5ff9e27e334e5022167
Anawin A. <138203645+AnawinA@users.noreply.github.com>
2025-03-31 01:38:43 +0700

    try to ignore nodebug experiment

---
+++ Demo/src/Main/NoDebug.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+/**
+ *
+ * @author anawi
+ */
+public class NoDebug {
+    public static boolean getDEBUG_MODE() {
+        return false;
+    }
+}
