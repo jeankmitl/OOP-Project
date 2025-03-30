12b95c59738973619dff156ff11584cc1391a2d1
Anawin A. <138203645+AnawinA@users.noreply.github.com>
2025-03-30 19:12:44 +0700

    you can choose Local or Keyboard in 2-players button

--- Demo/src/Main/MainMenu.java
+++ Demo/src/Main/MainMenu.java
@@ -2,6 +2,8 @@ package Main;
 
 import Asset.Audio;
 import Asset.AudioName;
+import Asset.ImgManager;
+import CoOpSystem.CoOpFrame;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
@@ -10,9 +12,12 @@ import javax.swing.*;
 
 public class MainMenu extends JFrame {
 
-    private Rectangle start, twoP, config, exit, menuBar, logo;
-    private Image menu, title, background;
+    private Rectangle start, twoP, config, exit, menuBar, logo, option2P, local, keyboard;
+    private Image menu, title, background, option2PImg;
     private boolean isButtonHovered = false;
+    private boolean isShowOption2P = false;
+    private boolean isHovered2Player = false;
+    
     private Timer timer;
     private MainMenuPanel mainMenuPanel;
     
@@ -25,12 +30,17 @@ public class MainMenu extends JFrame {
         config = new Rectangle(75, 350, 300, 115);
         exit = new Rectangle(75, 485, 300, 115);
         mainMenuPanel = new MainMenuPanel();
-
+        
+        option2P = new Rectangle(375, 179, 400, 175);
+        local = new Rectangle(option2P.x + 70, option2P.y, 270, 70);
+        keyboard = new Rectangle(option2P.x + 70, option2P.y + 100, 350, 70);
+        
         try {
             setIconImage(new ImageIcon(getClass().getResource("/Asset/Img/Icons/icon.png")).getImage());
             menu = ImageIO.read(getClass().getResource("/Asset/Img/Icons/MenuBar.PNG"));
             title = ImageIO.read(getClass().getResource("/Asset/Img/Icons/defense_of_the_dungeon_no_s.png"));
             background = ImageIO.read(getClass().getResource("/Asset/Img/Background/defense_of_dungeon_wallpaper.png"));
+            option2PImg = ImgManager.loadIcon("2-playere_2-options");
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -71,25 +81,10 @@ public class MainMenu extends JFrame {
                     worker.execute();
                 }
                 else if (twoP.contains(e.getPoint())) {
-                    Audio.play(AudioName.BUTTON_CLICK);
-
-                    SwingWorker<Void, Void> worker = new SwingWorker<>() {
-                        LoadingScreen loadingScreen = new LoadingScreen();
-                        
-                        @Override
-                        protected Void doInBackground() throws Exception {
-                            Thread.sleep(1000);
-                            new StageSelector("2p");
-                            dispose();
-                            return null;
-                        }
-
-                        @Override
-                        protected void done() {
-                            loadingScreen.dispose();
-                        }
-                    };
-                    worker.execute();
+                    if (!isShowOption2P) {
+                        isShowOption2P = true;
+                    }
+                    Audio.play(AudioName.BUTTON_HOVER);
                 }
                 else if (config.contains(e.getPoint())) {
                     Audio.play(AudioName.BUTTON_CLICK);
@@ -115,22 +110,70 @@ public class MainMenu extends JFrame {
                 else if (exit.contains(e.getPoint())) {
                     System.exit(0);
                 }
+                if (isShowOption2P) {
+                    if (local.contains(e.getPoint())) {
+                        Audio.play(AudioName.BUTTON_CLICK);
+                        SwingWorker<Void, Void> worker = new SwingWorker<>() {
+                            LoadingScreen loadingScreen = new LoadingScreen();
+
+                            @Override
+                            protected Void doInBackground() throws Exception {
+                                Thread.sleep(1000);
+                                new CoOpFrame();
+                                dispose();
+                                return null;
+                            }
+
+                            @Override
+                            protected void done() {
+                                loadingScreen.dispose();
+                            }
+                        };
+                        worker.execute();
+                    } else if (keyboard.contains(e.getPoint())) {
+                        Audio.play(AudioName.BUTTON_CLICK);
+                        SwingWorker<Void, Void> worker = new SwingWorker<>() {
+                            LoadingScreen loadingScreen = new LoadingScreen();
+
+                            @Override
+                            protected Void doInBackground() throws Exception {
+                                Thread.sleep(1000);
+                                new StageSelector("2p");
+                                dispose();
+                                return null;
+                            }
+
+                            @Override
+                            protected void done() {
+                                loadingScreen.dispose();
+                            }
+                        };
+                        worker.execute();
+                    }
+                }
             }
         });
 
         addMouseMotionListener(new MouseMotionAdapter() {
             @Override
             public void mouseMoved(MouseEvent e) {
-                if (start.contains(e.getPoint()) || twoP.contains(e.getPoint()) || config.contains(e.getPoint()) || exit.contains(e.getPoint())) {
+                if (start.contains(e.getPoint()) || twoP.contains(e.getPoint()) || config.contains(e.getPoint()) || exit.contains(e.getPoint()) ||
+                       (isShowOption2P && (local.contains(e.getPoint()) || keyboard.contains(e.getPoint()))) ) {
                     setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
                     if (!isButtonHovered) {
                         isButtonHovered = true;
                         Audio.play(AudioName.BUTTON_HOVER);
                     }
-                }
-                else {
+                } else {
                     setCursor(Cursor.getDefaultCursor());
                     isButtonHovered = false;
+                }
+                if ((twoP.contains(e.getPoint()))) {
+//                    isShowOption2P = true;
+                } else {
+                    if (!option2P.contains(e.getPoint())) {
+                        isShowOption2P = false;
+                    }
                 }
             }
         });
@@ -174,6 +217,7 @@ public class MainMenu extends JFrame {
             g2d.drawImage(background, 0, 0, 1280, 720,this);
             g2d.drawImage(menu, menuBar.x, menuBar.y, menuBar.width, menuBar.height,this);
             g2d.drawImage(title, logo.x, logo.y + (int)(20 * Math.sin(Math.toRadians(titleAnimY))), logo.width, logo.height,this);
+            if (isShowOption2P) g2d.drawImage(option2PImg, option2P.x, option2P.y, option2P.width, option2P.height, this);
         }
     }
 
